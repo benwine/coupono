e4a112e51c417b54991452cd41aecd64ecd3913a
Ben Wine <benwine@gmail.com>
2013-06-26 18:29:52 -0500

    hi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coupono::Application.config.secret_token = 'df9c9388e367417bb6ac4196fe6c899f06487fd5f6453f9bc8fe1183ce7c97171a2dee207ef435b6e8068d6500122934f7368f1d96c5231a65274a77f0133ecf'
